980aa585937d049b152b5e7b08c1e068faaaf378
James Halliday <mail@substack.net>
2012-05-18 03:16:31 -0700

    failing parse test

---
+++ test/parse.js
@@ -0,0 +1,12 @@
+var test = require('tap').test;
+var parse = require('../').parse;
+
+test('parse shell commands', function (t) {
+    t.same('a \'b\' "c"', [ 'a', 'b', 'c' ]);
+    
+    t.same(
+        parse('beep "boop" \'foo bar baz\' "it\'s \\"so\\" groovy"'),
+        [ 'beep', 'boop', 'foo bar baz', 'it\'s "so" groovy' ]
+    );
+    t.end();
+});
