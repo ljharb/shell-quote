8dbb2803136a2f7643e7e66b2d6b95f9adfbfd41
James Halliday <mail@substack.net>
2012-05-28 15:55:23 -0700

    failing test for: echo "foo = \"foo\""

--- test/parse.js
+++ test/parse.js
@@ -9,6 +9,7 @@ test('parse shell commands', function (t) {
     );
     t.same(parse('a b\\ c d'), [ 'a', 'b c', 'd' ]);
     t.same(parse('\\$beep bo\\`op'), [ '$beep', 'bo`op' ]);
+    t.same(parse('echo "foo = \"foo\""'), [ 'echo', 'foo = "foo"' ]);
     
     t.end();
 });
