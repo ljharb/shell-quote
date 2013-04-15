37d60580a4a4656ff836c4a2ecdd7282705ffd27
James Halliday <mail@substack.net>
2013-04-14 20:36:59 -0700

    externalize the regex declaration

--- index.js
+++ index.js
@@ -12,22 +12,24 @@ exports.quote = function (xs) {
     }).join(' ');
 };
 
+var re = {
+    chunk: /(['"])((\\\1|[^\1])*?)\1|(\\ |\S)+/g
+};
+
 exports.parse = function (s, env) {
     if (!env) env = {};
-    return s.match(/(['"])((\\\1|[^\1])*?)\1|(\\ |\S)+/g)
-        .map(function (s) {
-            if (/^'/.test(s)) {
-                return s
-                    .replace(/^'|'$/g, '')
-                    .replace(/\\(["'\\$`(){}!#&*|])/g, '$1')
-                ;
-            }
-            else if (/^"/.test(s)) {
-                return interpolate(s.replace(/^"|"$/g, ''));
-            }
-            else return interpolate(s);
-        })
-    ;
+    return s.match(re.chunk).map(function (s) {
+        if (/^'/.test(s)) {
+            return s
+                .replace(/^'|'$/g, '')
+                .replace(/\\(["'\\$`(){}!#&*|])/g, '$1')
+            ;
+        }
+        else if (/^"/.test(s)) {
+            return interpolate(s.replace(/^"|"$/g, ''));
+        }
+        else return interpolate(s);
+    });
     
     function interpolate (s) {
         return s
