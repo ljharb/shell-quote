17d1fdac759e7a94ffc6edc26af27769d30e53bb
James Halliday <mail@substack.net>
2013-04-14 19:46:31 -0700

    fixed a failing env test

--- test/env.js
+++ test/env.js
@@ -4,7 +4,7 @@ var parse = require('../').parse;
 test('expand environment variables', function (t) {
     t.same(parse('a $XYZ c', { XYZ: 'b' }), [ 'a', 'b', 'c' ]);
     t.same(parse('a${XYZ}c', { XYZ: 'b' }), [ 'abc' ]);
-    t.same(parse('a${XYZ}c $XYZ', { XYZ: 'b' }), [ 'abcb' ]);
+    t.same(parse('a${XYZ}c $XYZ', { XYZ: 'b' }), [ 'abcb b' ]);
     t.same(parse('"_$X-$Y_"', { X: 'a', Y: 'b' }), [ '_a-b_' ]);
     t.same(parse("'_$X-$Y_'", { X: 'a', Y: 'b' }), [ '_$X-$Y_' ]);
     t.same(parse('qrs"$zzz"wxy', { zzz: 'tuv' }), [ 'qrstuvwxy' ]);
