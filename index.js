589147176be93834bad7fcee83bd255e35c14adc
James Halliday <mail@substack.net>
2013-04-14 20:08:26 -0700

    half the env tests are working with basic interpolation

--- index.js
+++ index.js
@@ -12,22 +12,34 @@ exports.quote = function (xs) {
     }).join(' ');
 };
 
-exports.parse = function (s) {
+exports.parse = function (s, env) {
+    if (!env) env = {};
     return s.match(/(['"])((\\\1|[^\1])*?)\1|(\\ |\S)+/g)
         .map(function (s) {
             if (/^'/.test(s)) {
                 return s
                     .replace(/^'|'$/g, '')
-                    .replace(/\\(["'\\$`(){}!#&*|])/g, '$1');
+                    .replace(/\\(["'\\$`(){}!#&*|])/g, '$1')
                 ;
             }
             else if (/^"/.test(s)) {
                 return s
                     .replace(/^"|"$/g, '')
-                    .replace(/\\(["'\\$`(){}!#&*|])/g, '$1');
+                    .replace(/\\(["'\\$`(){}!#&*|])/g, '$1')
+                    .replace(/\$(\w+)/g, getVar)
+                    .replace(/\${(\w+)}/g, getVar)
                 ;
             }
-            else return s.replace(/\\([ "'\\$`(){}!#&*|])/g, '$1');
+            else {
+                return s
+                    .replace(/\\([ "'\\$`(){}!#&*|])/g, '$1')
+                    .replace(/\$(\w+)/g, getVar)
+                    .replace(/\${(\w+)}/g, getVar)
+                ;
+            }
         })
     ;
+    function getVar (_, x) {
+        return String(env[x])
+    }
 };
