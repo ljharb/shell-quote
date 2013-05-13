710bb243f23d4a55158688b71cb56b67f66ea99f
James Halliday <mail@substack.net>
2013-05-13 03:09:29 -0700

    failing expanded single-op tests for ; and ()

--- test/op.js
+++ test/op.js
@@ -1,15 +1,21 @@
 var test = require('tape');
 var parse = require('../').parse;
 
-test('operators', function (t) {
+test('single operators', function (t) {
     t.same(parse('beep | boop'), [ 'beep', { op: '|' }, 'boop' ]);
     t.same(parse('beep|boop'), [ 'beep', { op: '|' }, 'boop' ]);
     t.same(parse('beep \\| boop'), [ 'beep', '|', 'boop' ]);
     t.same(parse('beep "|boop"'), [ 'beep', '|boop' ]);
+    
     t.same(parse('echo zing &'), [ 'echo', 'zing', { op: '&' } ]);
     t.same(parse('echo zing&'), [ 'echo', 'zing', { op: '&' } ]);
     t.same(parse('echo zing\\&'), [ 'echo', 'zing&' ]);
     t.same(parse('echo "zing\\&"'), [ 'echo', 'zing&' ]);
+    
+    t.same(parse('beep;boop'), [ 'beep', { op: ';' }, 'boop' ]);
+    t.same(parse('(beep;boop)'), [
+        { op: '(' }, 'beep', { op: ';' }, 'boop', { op: ')' }
+    ]);
     
     t.end();
 });
