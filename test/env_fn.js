914a1a9ec55cd76bedfed4086c35866733128036
James Halliday <mail@substack.net>
2013-05-13 06:38:30 -0700

    another check just to make sure env() works

--- test/env_fn.js
+++ test/env_fn.js
@@ -2,10 +2,11 @@ var test = require('tape');
 var parse = require('../').parse;
 
 test('functional env expansion', function (t) {
-    t.plan(2);
+    t.plan(3);
     
     t.same(parse('a $XYZ c', getEnv), [ 'a', 'xxx', 'c' ]);
     t.same(parse('a $XYZ c', getEnvObj), [ 'a', { op: '@@' }, 'c' ]);
+    t.same(parse('a${XYZ}c', getEnvObj), [ 'a', { op: '@@' }, 'c' ]);
     
     function getEnv (key) {
         return 'xxx';
