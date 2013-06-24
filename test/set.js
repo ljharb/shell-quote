7d5636bec5e76ff542712e07643b09c597d20b21
James Halliday <mail@substack.net>
2013-06-24 04:17:52 -0700

    updated set env test, already passes

--- test/set.js
+++ test/set.js
@@ -4,15 +4,15 @@ var parse = require('../').parse;
 test('set env vars', function (t) {
     t.same(
         parse('ABC=444 x y z'),
-        [ { key: 'X', value: '444' }, 'x', 'y', 'z' ]
+        [ 'ABC=444', 'x', 'y', 'z' ]
     );
     t.same(
         parse('ABC=3\\ 4\\ 5 x y z'),
-        [ { key: 'X', value: '3 4 5' }, 'x', 'y', 'z' ]
+        [ 'ABC=3 4 5', 'x', 'y', 'z' ]
     );
     t.same(
         parse('X="7 8 9" printx'),
-        [ { key: 'X', value: '7 8 9' }, 'printx' ]
+        [ 'X=7 8 9', 'printx' ]
     );
     
     t.end();
