c99dca59dca64743877a0411d299ce669f0a2d1d
James Halliday <mail@substack.net>
2014-07-20 14:26:13 -0700

    all the falseys test

--- test/quote.js
+++ test/quote.js
@@ -13,6 +13,7 @@ test('quote', function (t) {
     );
     t.equal(quote([]), '');
     t.equal(quote([ 'a', 1, true, false ]), 'a 1 true false');
+    t.equal(quote([ 'a', 1, null, undefined ]), 'a 1 null undefined');
     t.end();
 });
 
