257a25cdd29daca05885eda9c6e60150b0376dfa
Radu Hociung <radu.git@Mergesium.com>
2023-10-30 05:31:38 -0400

    Fix quote - was severely broken
    The quote function was severely broken:
    
    1. operators were quoted; should have been output literally
    
    2. backslashes were used as escapes inside single-quoted strings
       (they lose the escape meaning inside single quoted strings)
    
    3. Ugliness - when no quoting was needed, it was applied anyway
       examples:
       - VAR=value needs no quoting. it was output as VAR\=value
       - main^{commit} needs no quoting. was output as main\^\{commit\}
    
    Fixes Mergesium/node-shell-quote#1 and ljharb/shell-quote#11
    
    Author-Rebase-Consent: https://No-rebase.github.io

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 exports.quote = function (xs) {
     return xs.map(function (s) {
         if (s && typeof s === 'object') {
-            return s.op.replace(/(.)/g, '\\$1');
+            return s.op;
         }
-        else if (/["\s]/.test(s) && !/'/.test(s)) {
-            return "'" + s.replace(/(['\\])/g, '\\$1') + "'";
+        // Enclose strings with metacharacters in single quoted,
+        // and escape any single quotes.
+        // Match strictly to avoid escaping things that don't need to be.
+        // bash: |  & ; ( ) < > space tab
+        // Also escapes bash curly brace ranges {a..b} {a..z..3} {1..20} {a,b} but not
+        // {a...b} or {..a}
+        if ((/(?:["\\$`!\s|&;\(\)<>]|{[\d]+\.{2}[\d]+(?:\.\.\d+)?}|{[a-zA-Z].{2}[a-zA-Z](?:\.\.\d+)?}|{[^{]*,[^}]*})/m).test(s)) {
+            // If input contains outer single quote, escape each of them individually.
+            // eg. 'a b c' -> \''a b c'\'
+            var outer_quotes = s.match(/^('*)(.*?)('*)$/s);
+
+            // the starting outer quotes individually escaped
+            return String(outer_quotes[1]).replace(/(.)/g, '\\$1') +
+                // the text inside the outer single quotes is single quoted
+                "'" + outer_quotes[2].replace(/'/g, '\'\\\'\'') + "'" +
+                // the ending outer quotes individually escaped
+                String(outer_quotes[3]).replace(/(.)/g, '\\$1');
         }
-        else if (/["'\s]/.test(s)) {
-            return '"' + s.replace(/(["\\$`(){}!#&*|])/g, '\\$1') + '"';
-        }
-        else {
-            return s.replace(/([\\$`(){}!#&*|])/g, '\\$1');
-        }
+        // Only escape the single quotes in strings without metachars or
+        // separators
+        return String(s).replace(/(')/g, '\\$1');
     }).join(' ');
 };
 
